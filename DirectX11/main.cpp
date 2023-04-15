b6f58acbb36d63581e81131479d07749c7e64627
congcong-hehe <heyaqihehe@163.com>
2023-04-15 14:54:46 +0800

    first commit

---
+++ DirectX11/main.cpp
@@ -0,0 +1,10 @@
+#include "WinMain.h"
+
+#include <stdio.h>
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE prevInstance,
+    _In_ LPSTR cmdLine, _In_ int showCmd)
+{
+    printf("Hello world!");
+    return 0;
+}
